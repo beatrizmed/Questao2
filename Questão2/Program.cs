40fc67c4d0f1c25fd38949436e2fcbd4bb8e8f83
Beatriz Medeiros <anbeatriz1@hotmail.com>
2024-03-06 23:25:38 -0300

    Adicionar arquivos de projeto.

---
+++ Questão2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Digite um número para verificar se pertence à sequência de Fibonacci:");
+		int numero = int.Parse(Console.ReadLine());
+
+		if (VerificarSePertenceFibonacci(numero))
+		{
+			Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+			Console.ReadKey();
+		}
+		else
+		{
+			Console.WriteLine($"{numero} NÃO pertence à sequência de Fibonacci.");
+			Console.ReadKey();
+		}
+	}
+
+	static bool VerificarSePertenceFibonacci(int numero)
+	{
+		int a = 0;
+		int b = 1;
+		int soma = 0;
+
+		while (soma < numero)
+		{
+			soma = a + b;
+			a = b;
+			b = soma;
+		}
+
+		return soma == numero;
+	}
+}
